c3eb79464affac5330ea9b36bd2e0a83f4698f97
marcosrgarc <marcosrgarc@ext.inditex.com>
2023-11-01 21:01:29 +0100

    Commit inicial Ejercicio 2

---
+++ Ejercicio_2/src/main/java/com/curso/inicio/Ejercicio2Application.java
@@ -0,0 +1,13 @@
+package com.curso.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio2Application.class, args);
+	}
+
+}
