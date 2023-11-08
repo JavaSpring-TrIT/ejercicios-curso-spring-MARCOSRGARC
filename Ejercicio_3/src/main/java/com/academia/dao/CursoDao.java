00e912525955dd5d5299df8e4a5874801fab64eb
marcosrgarc <marcosrgarc@ext.inditex.com>
2023-11-08 09:50:06 +0100

    Commit inicial Ejercicio 3

---
+++ Ejercicio_3/src/main/java/com/academia/dao/CursoDao.java
@@ -0,0 +1,17 @@
+package com.academia.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.academia.model.Curso;
+
+public interface CursoDao extends JpaRepository <Curso, String> {
+	
+	
+	 @Query(value = "SELECT * from curso" +
+		      " WHERE precio >= ?1 AND  precio <= ?2 ; ", nativeQuery = true)
+		  List<Curso> findByprecio(int pMin, int pMax);
+
+}
