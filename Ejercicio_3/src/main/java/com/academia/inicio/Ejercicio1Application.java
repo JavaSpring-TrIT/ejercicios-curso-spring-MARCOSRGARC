e4928cf50c7213f857e1964e9d0a9f0f64fff2f9
marcosrgarc <marcosrgarc@ext.inditex.com>
2023-11-09 13:54:29 +0100

    Completado Ejercicio 3

--- Ejercicio_3/src/main/java/com/academia/inicio/Ejercicio1Application.java
+++ Ejercicio_3/src/main/java/com/academia/inicio/Ejercicio1Application.java
@@ -1,14 +1,14 @@
 package com.academia.inicio;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages= "com.academia.model")
 @EnableJpaRepositories(basePackages = "com.academia.dao")
-@SpringBootApplication(scanBasePackages = {"com.academia.controller","com.academia.service"})
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class},scanBasePackages = {"com.academia.controller","com.academia.service"})
 public class Ejercicio1Application {
 
 	public static void main(String[] args) {
